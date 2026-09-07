fa03021d2df8224f1c6222211f5c17e5449e2224
robhuk <roberthinds@gmail.com>
2026-09-07 17:20:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE="route-card-v3";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon.jpeg","./xc-white-logo.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="route-card-pwa-v4";
+const ASSETS=["./","./index.html","./manifest.json","./icon.jpeg","./xc-white-logo.png"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).catch(()=>caches.match("./index.html"))));
+});
