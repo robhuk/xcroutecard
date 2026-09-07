cd29c05bcea747d13252d52824c8bfa3bc7c3f3d
robhuk <roberthinds@gmail.com>
2026-09-07 17:30:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="route-card-pwa-v6";
+const CACHE="route-card-pwa-v7";
 const ASSETS=["./","./index.html","./manifest.json","./sw.js","./icon.jpeg","./xc-white-logo.png"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
